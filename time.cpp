6afb80f6140b44f1d2891f6677ca7f64e0115770
pyvyx <77646182+pyvyx@users.noreply.github.com>
2023-12-21 19:15:09 +0100

    Add files via upload

--- time.cpp
+++ time.cpp
@@ -3,12 +3,14 @@
 #include <string>
 #include <cstdint>
 #include <cinttypes>
+#include <algorithm>
+#include <unordered_map>
 
 #define UNICODE
+#define NOMINMAX
 #include <Windows.h>
 
-#define DECIMAL_PRECISION  3
-#define FORMAT_TOTAL_WIDTH 5 + DECIMAL_PRECISION
+#define DECIMAL_PRECISION 3
 #define PRINT_SECONDS_END(time) printf(" (%.*f sec)\n", DECIMAL_PRECISION, time * 0.000000001)
 
 std::string error_as_string()
@@ -34,35 +36,77 @@ std::string error_as_string()
 }
 
 
-void print_time(uint64_t time, const char* fmt)
+double get_conversion(uint64_t time)
 {
-    if (time < 1000) // nanoseconds
+    if (time < 1e6 && time >= 1000) // microseconds
     {
-        printf("%s %*" PRId64 " ns", fmt, 4, time);
-        PRINT_SECONDS_END(time);
-    }
-    else if (time < 1e6) // microseconds
-    {
-        printf("%s %*.*f mcs", fmt, FORMAT_TOTAL_WIDTH, DECIMAL_PRECISION, time * 0.001);
-        PRINT_SECONDS_END(time);
+        return 0.001;
     }
     else if (time < 1e9) // milliseconds
     {
-        printf("%s %*.*f ms", fmt, FORMAT_TOTAL_WIDTH, DECIMAL_PRECISION, time * 0.000001);
-        PRINT_SECONDS_END(time);
+        return 0.000001;
     }
     else if (time < 6e10) // seconds
     {
-        printf("%s %*.*f sec\n", fmt, FORMAT_TOTAL_WIDTH, DECIMAL_PRECISION, time * 0.000000001);
+        return 0.000000001;
     }
     else if (time < 3.6e12) // minutes
     {
-        printf("%s %*.*f min", fmt, FORMAT_TOTAL_WIDTH, DECIMAL_PRECISION, time * 1.6666666666667E-11);
-        PRINT_SECONDS_END(time);
+        return 1.6666666666667E-11;
     }
     else // if (time < 8.64e+13) // hours
     {
-        printf("%s %*.*f h", fmt, FORMAT_TOTAL_WIDTH, DECIMAL_PRECISION, time * 2.777777777E-13);
+        return 2.777777777E-13;
+    }
+    return 1.0;
+}
+
+
+const char* get_conversion_format(double conversion)
+{
+    if (conversion == 0.001)
+        return "mcs";
+    if (conversion == 0.000001)
+        return "ms";
+    if (conversion == 1.6666666666667E-11)
+        return "m";
+    if (conversion == 2.777777777E-13)
+        return "h";
+    return "";
+}
+
+
+int max_time_width(uint64_t time, uint64_t time2, uint64_t time3, uint64_t time4)
+{
+    const size_t timeWidth = std::to_string(static_cast<uint64_t>(time * get_conversion(time))).size();
+    const size_t time2Width = std::to_string(static_cast<uint64_t>(time2 * get_conversion(time2))).size();
+    const size_t time3Width = std::to_string(static_cast<uint64_t>(time3 * get_conversion(time3))).size();
+    const size_t time4Width = std::to_string(static_cast<uint64_t>(time4 * get_conversion(time4))).size();
+
+    size_t maxWidth = std::max(timeWidth, time2Width);
+    maxWidth = std::max(maxWidth, time3Width);
+    return (int)std::max(maxWidth, time4Width);
+}
+
+
+
+void print_time(uint64_t time, const char* fmt, int formatWidth)
+{
+    if (time < 1000) // nanoseconds
+    {
+        printf("%s %" PRId64 " ns", fmt, time);
+        PRINT_SECONDS_END(time);
+        return;
+    }
+
+    const double conversion = get_conversion(time);
+    if (conversion == 0.000000001)
+    {
+        printf("%s %*.*f sec\n", fmt, formatWidth + 1 + DECIMAL_PRECISION, DECIMAL_PRECISION, time * conversion);
+    }
+    else
+    {
+        printf("%s %*.*f %s", fmt, formatWidth + 1 + DECIMAL_PRECISION, DECIMAL_PRECISION, time * conversion, get_conversion_format(conversion));
         PRINT_SECONDS_END(time);
     }
 }
@@ -106,12 +150,14 @@ void time_app(LPWSTR args)
     // Calculate CPU time in nanoseconds
     const uint64_t userTimeFinal = userTimeInt.QuadPart * 100;
     const uint64_t kernelTimeFinal = kernelTimeInt.QuadPart * 100;
+    const uint64_t totalCpuTime = kernelTimeFinal + userTimeFinal;
     const uint64_t realTime = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(t_end - t_start).count());
     puts("");
-    print_time(realTime,                        "Real time:     ");
-    print_time(userTimeFinal,                   "User time:     ");
-    print_time(kernelTimeFinal,                 "Kernel time:   ");
-    print_time(kernelTimeFinal + userTimeFinal, "Total CPU time:");
+    const int maxWidth = max_time_width(realTime, userTimeFinal, kernelTimeFinal, totalCpuTime);
+    print_time(realTime,        "Real time:     ", maxWidth);
+    print_time(userTimeFinal,   "User time:     ", maxWidth);
+    print_time(kernelTimeFinal, "Kernel time:   ", maxWidth);
+    print_time(totalCpuTime,    "Total CPU time:", maxWidth);
 }
 
 
