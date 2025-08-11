1df6b6fe340347d867e925bbb16b036187272625
cunterrainer <vyx3327@web.de>
2025-08-11 03:32:29 +0200

    Added more detailed output

--- hash_benchmark.cpp
+++ hash_benchmark.cpp
@@ -1,17 +1,19 @@
 #include <string_view>
 #include <functional>
 #include <iostream>
-#include <thread>
 #include <vector>
 #include <random>
 #include <string>
+#include <format>
 #include <utility>
 
 #include "Hash.h"
 #include "Profiler.h"
 
-#define NUMBER_OF_STRINGS 10000
-#define MAX_STRING_SIZE   10000
+#define NUMBER_OF_STRINGS   10000
+#define MAX_STRING_SIZE     10000
+#define GENERATE_RND_LENGTH 0
+#define WARMUP_RUNS         2  // To warm up the cache and branch predictor
 
 typedef std::vector<char> char_array;
 char_array charset()
@@ -43,7 +45,7 @@ std::string random_string(size_t length, std::function<char(void)> rand_char)
 }
 
 
-std::vector<std::string> GenerateStrings(size_t count)
+std::vector<std::string> GenerateStrings(size_t count, size_t* total_bytes)
 {
     const auto ch_set = charset();
 
@@ -63,7 +65,12 @@ std::vector<std::string> GenerateStrings(size_t count)
 
     for (size_t i = 0; i < count; ++i)
     {
-        strings.emplace_back(random_string(rnum(0, MAX_STRING_SIZE), randchar));
+        #if GENERATE_RND_LENGTH
+            strings.emplace_back(random_string(rnum(0, MAX_STRING_SIZE), randchar));
+        #else
+            strings.emplace_back(random_string(MAX_STRING_SIZE, randchar));
+        #endif
+        *total_bytes += strings.back().size();
     }
     return strings;
 }
@@ -72,33 +79,64 @@ std::vector<std::string> GenerateStrings(size_t count)
 int main()
 {
     Profiler::Start();
+    size_t total_bytes = 0;
     std::vector<std::pair<double, std::string>> times;
-    const std::vector<std::string> strings = GenerateStrings(NUMBER_OF_STRINGS);
+    const std::vector<std::string> strings = GenerateStrings(NUMBER_OF_STRINGS, &total_bytes);
 
     auto Measure = [&](std::string (*hashfunc)(std::string_view str), std::string name)
     {
-        for (const std::string& s : strings)
+        for (int i = 0; i < WARMUP_RUNS; ++i)
         {
-            Profiler::Start();
-            hashfunc(s);
-            Profiler::End();
+            for (const std::string& s : strings)
+            {
+                hashfunc(s);
+            }
         }
-        times.push_back({Profiler::Average(Profiler::Conversion::Milliseconds), name});
-        std::cout << name.append(14 - name.size(), ' ') << ": ";
-        Profiler::Log(Profiler::Conversion::Milliseconds);
+
+        Profiler::Reset();
+        const int strings_size = strings.size();
+        
+        Profiler::Start();
+        for (int i = 0; i < strings_size; ++i)
+        {
+            hashfunc(strings[i]);
+        }
+        Profiler::End();
+
+        const double seconds = Profiler::Total(Profiler::Conversion::Seconds);
+        const double gb_per_second = (total_bytes / 1e9) / seconds;
+        const double average_time = Profiler::Total(Profiler::Conversion::Nanoseconds) / strings_size;
+
+        times.push_back({average_time, name});
+        std::cout << std::format("{:<14}: {:>8.8f} GB/s  Nanoseconds per hash: {:>8.1f}  Execution time: {:>8.3f} ms  Hashes calculated: {}", name, gb_per_second, average_time, Profiler::Total(Profiler::Conversion::Milliseconds), strings_size) << std::endl;
     };
 
     auto MeasureChar = [&](const char* (*hashfunc)(const char* str), std::string name)
     {
-        for (const std::string& s : strings)
+        for (int i = 0; i < WARMUP_RUNS; ++i)
         {
-            Profiler::Start();
-            hashfunc(s.c_str());
-            Profiler::End();
+            for (const std::string& s : strings)
+            {
+                hashfunc(s.c_str());
+            }
         }
-        times.push_back({Profiler::Average(Profiler::Conversion::Milliseconds), name});
-        std::cout << name.append(14 - name.size(), ' ') << ": ";
-        Profiler::Log(Profiler::Conversion::Milliseconds);
+
+        Profiler::Reset();
+        const int strings_size = strings.size();
+
+        Profiler::Start();
+        for (int i = 0; i < strings_size; ++i)
+        {
+            hashfunc(strings[i].c_str());
+        }
+        Profiler::End();
+
+        const double seconds = Profiler::Total(Profiler::Conversion::Seconds);
+        const double gb_per_second = (total_bytes / 1e9) / seconds;
+        const double average_time = Profiler::Total(Profiler::Conversion::Nanoseconds) / strings_size;
+
+        times.push_back({average_time, name});
+        std::cout << std::format("{:<14}: {:>8.8f} GB/s  Nanoseconds per hash: {:>8.1f}  Execution time: {:>8.3f} ms  Hashes calculated: {}", name, gb_per_second, average_time, Profiler::Total(Profiler::Conversion::Milliseconds), strings_size) << std::endl;
     };
 
     Profiler::End();
@@ -135,7 +173,7 @@ int main()
     MeasureChar(hash_md5_easy, "C MD5");
 
     std::cout << "\nStrings: " << NUMBER_OF_STRINGS << ", Max chars per string: " << MAX_STRING_SIZE << std::endl;
-    
+
     std::pair<double, std::string> fastest = times[0];
     std::pair<double, std::string> slowest = times[0];
     for (const auto& p : times)
@@ -144,6 +182,6 @@ int main()
         if (p.first > slowest.first) slowest = p;
     }
     
-    std::cout << "Winner: " << fastest.second << " " << fastest.first << std::endl;
+    std::cout << "Fastest: " << fastest.second << " " << fastest.first << std::endl;
     std::cout << "Slowest: " << slowest.second << " " << slowest.first << std::endl;
 }
